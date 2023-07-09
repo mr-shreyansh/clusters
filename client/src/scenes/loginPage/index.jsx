273b7128e0b109a6243459e1f1f29f403e2597ae
shreyansh <mrshreyansh176@gmail.com>
2023-07-09 18:16:21 +0530

    loginpage title changed to clusters

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
   return (
     <div className='bg-[#f9f6f6] dark:bg-stone-950 min-h-screen'>
       <div className='flex justify-center items-center bg-white min-h-[10vh]'>
-      <h1 className='text-3xl font-bold select-none text-cyan-500'>Sociopedia</h1>
+      <h1 className='text-3xl font-bold select-none text-cyan-500'>Clusters</h1>
       </div>
       <Form />
     </div>
